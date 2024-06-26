0baa6887fd989fa58b0ccf19b9d69228226daecb
Russellier <russellvap13@gmail.com>
2024-06-26 11:47:51 +0800

    refactor: Clean up code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,8 @@
 const repeatString = function(string, num) {
   let repeatedString = '';
-  if (num < 0) {
-    repeatedString ='ERROR';
-  } else {
-    for (let i = 0; i < num; i++) {
-      repeatedString += string;
-    }
+  if (num < 0) return 'ERROR';
+  for (let i = 0; i < num; i++) {
+    repeatedString += string;
   }  
   return repeatedString;
 };
