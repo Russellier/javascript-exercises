20356b24e5b020287d7ab8561f25e1142a70623b
Russellier <russellvap13@gmail.com>
2024-06-26 13:40:49 +0800

    fix: Edit code to pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, toRemove) {
+  
+  for (let i = 1; i <= arguments.length; i++) {
+    array = array.filter((item) => {
+      return item !== arguments[i];
+    })
+  }
+  return array;
 
+};
 // Do not edit below this line
 module.exports = removeFromArray;
