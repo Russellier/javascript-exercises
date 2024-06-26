208e4a85d8237468e8ecf99dd1679f6f78bc3aed
Russellier <russellvap13@gmail.com>
2024-06-26 14:48:13 +0800

    fix: Edit code to pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp1) {
+  return +(Number((temp1 - 32) * (5 / 9))).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp2) {
+  return +(Number((temp2 * (9 / 5)) + 32)).toFixed(1);
 };
 
 // Do not edit below this line
