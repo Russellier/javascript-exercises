0e5afc1542f92200f469b319a561daff65d00fa6
Russellier <russellvap13@gmail.com>
2024-06-26 12:03:40 +0800

    fix: Edit code to pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(originalText) {
+  let reversedText = '';
+  for (i = (originalText.length - 1); i >= 0; i--) {
+    reversedText += originalText.charAt(i);
+  }
+  return reversedText;
 };
 
 // Do not edit below this line
