f891588c0a018463caad76b21fa3b97187ca86d9
Russellier <russellvap13@gmail.com>
2024-07-09 15:08:03 +0800

    feat: Complete function to pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let original = string
+                  .split('')
+                  .filter(char => {
+                    return /[a-zA-Z0-9 ]/.test(char);
+                  })
+                  .join('')
+                  .toLowerCase()
+                  .split(' ')
+                  .join('');
+  let reversed = original
+                  .split('')
+                  .reverse()
+                  .join('');
+  return original === reversed;
 };
 
 // Do not edit below this line
