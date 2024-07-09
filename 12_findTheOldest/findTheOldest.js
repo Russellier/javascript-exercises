9df26899360cf882c2f2056aec5b7c8d0c5b1b75
Russellier <russellvap13@gmail.com>
2024-07-09 15:46:30 +0800

    feat: Complete function to pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  const oldest = array.sort((a, b) => {
+    if (!a.yearOfDeath) a.yearOfDeath = 2024;
+    if (!b.yearOfDeath) b.yearOfDeath = 2024;
+    const prev = a.yearOfDeath - a.yearOfBirth;
+    const next = b.yearOfDeath - b.yearOfBirth;
+    return prev > next ? -1 : 1;
+  });
+  return oldest[0];
 };
 
 // Do not edit below this line
