2d10c07841b6fe56b40dd992896c4df037ff9f2b
Russellier <russellvap13@gmail.com>
2024-07-09 14:29:15 +0800

    feat: Complete all functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, number) => (total + number), 0);
+  
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, number) => (product * number), 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) return 1;
+  let answer = 1;
+  for (let i = 1; i <= a; i++) {
+    answer *= i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
